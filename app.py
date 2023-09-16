0975cf2883df3921c88f3b3aa262fb7ecd85e9dc
root <kamal.guptha@ibm.com>
2023-09-16 18:39:18 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+        return "Hellow World!! This is a simple Flask app!"
+
+@app.route('/wassup')
+def hello():
+        return "Life is Fantastic. Isn't it?"
+
+if __name__ == "__main__":
+        app.run()
+
